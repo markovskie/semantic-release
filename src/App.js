41cdd875f733fe62b7ec8db0e2ff825d50b308b8
Borko <borko.novachevski@contractor.evgo.com>
2022-08-26 12:44:42 +0200

    fix(sudo): sudo

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
         Hello Niki !!!
-        Ema devops girl !!!
+        Ema DEVOPS girl !!!
         </p>
         <a
           className="App-link"
